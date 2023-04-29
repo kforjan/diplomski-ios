85c820a8a7fb99a14a880d6db82fb100599095fe
Kresimir <kresimir.forjan@axxessio.com>
2023-04-29 15:09:54 +0200

    Initial Commit

---
+++ iOSComparisonApp/iOSComparisonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSComparisonAppApp.swift
+//  iOSComparisonApp
+//
+//  Created by UHP on 29.04.2023..
+//
+
+import SwiftUI
+
+@main
+struct iOSComparisonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
